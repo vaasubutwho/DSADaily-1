862485a5ef42439932474709892e8a62f205089e
Sarthak Srivastava <sarthak.srivastava2019@vitstudent.ac.in>
2020-12-20 20:22:45 +0000

    code c

--- main.cpp
+++ main.cpp
@@ -12,5 +12,24 @@ void push(node** headRef, int data){
   *headRef=newNode;
 }
 void deleteNode(node** headRef, int key){
+  node* temp= *headRef;
+  node* prev=NULL;
+  if(temp!=NULL && temp->data==key){
+    *headRef=temp->next;
+    delete temp;
+    return;
+  }
+  while(temp!=NULL && temp->data!=key){
+    prev=temp;
+    temp=temp->next;
+  }
+  if(temp==NULL){
+    return;
+
+  }
+  prev->next=temp->next;
+  delete temp;
+}
+void printList(node** headRef){
   
 }
